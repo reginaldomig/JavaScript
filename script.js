12eab86c9092f6149c905ed353d3a605b3aa835d
ReginaldoMiguel <reginaldomig@gmail.com>
2023-03-19 11:26:33 -0300

    u

---
+++ script.js
@@ -0,0 +1,13 @@
+var tabuada = 7
+var porQuanto = 0
+
+var contador = 11
+
+
+while (contador > 0){
+    res = tabuada * porQuanto
+    console.log(`${tabuada} X ${porQuanto} = ${res}`)
+    contador = contador -1
+    porQuanto = porQuanto +1
+}
+
