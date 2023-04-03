37f4512fdfaafdd91c23fd07eaf4695c9d1aeec3
ReginaldoMiguel <reginaldomig@gmail.com>
2023-04-02 22:37:30 -0300

    s

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var hora = new Date().getHours()
 
+// das 0 às 11:59       = Bom dia
+// das 12 às 17:59      = Boa tarde
+// das 18 às 23:59      = Boa noite
+
 if (hora >= 0 && hora <=12) {
    console.log("Bom dia!") 
    hora = hora +1
@@ -12,4 +16,5 @@ if (hora >=12 && hora <18) {
 if (hora >=18) {
     console.log("Boa noite!")
     hora = hora +1
+    
 }
